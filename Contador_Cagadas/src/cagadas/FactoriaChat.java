5e313c490832107c7bd5bc0b0e711b43244f6fc6
dlozaco <dlozaco975@gmail.com>
2024-07-02 01:33:14 +0200

    Lectura terminada, falta probar

--- Contador_Cagadas/src/cagadas/FactoriaChat.java
+++ Contador_Cagadas/src/cagadas/FactoriaChat.java
@@ -10,9 +10,7 @@ public class FactoriaChat {
 	
 	public static Chat crearChat(String ruta) {
 		Stream<Mensaje> mensajes = null;
-		
 		try {
-			
 			mensajes = Files.lines(Paths.get(ruta))
 					.skip(1)
 					.map(FactoriaChat::parseaMensaje);
@@ -20,18 +18,29 @@ public class FactoriaChat {
 		} catch(Exception e) {
 			System.out.println("Error al procesar el archivo: "+e);
 		}
-		
 		return new Chat(mensajes);
-
 	}
 	
 	private static Mensaje parseaMensaje(String linea) {
 		String[] trozos = linea.split("-");
 		DateTimeFormatter formato = DateTimeFormatter.ofPattern("D/m/Y, HH:MM");
 		LocalDateTime fechaHora = LocalDateTime.parse(trozos[0].trim(),formato);
+		String[] texto = parseaTexto(trozos[1].trim());
+		String nombre = texto[0].trim();
+		String mensaje = texto[1].trim();
+		return new Mensaje(fechaHora, nombre, mensaje);
+	}
+
+	private static String[] parseaTexto(String texto) {
+		String [] trozos2 = null;
+		try {
+			trozos2 = texto.split(":");
+			
+		}catch(Exception e) {
+			trozos2 = texto.split("");
+		}
+		return trozos2;
 		
-		
-		return null;
 	}
 
 }
