7e239a8bf6bc347970d4e92ffb1f4a0b1ea34dc4
dlozaco <dlozaco975@gmail.com>
2024-07-03 00:14:01 +0200

    Comprobar moderación

--- Contador_Cagadas/src/cagadas/Mensaje.java
+++ Contador_Cagadas/src/cagadas/Mensaje.java
@@ -1,7 +1,34 @@
 package cagadas;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public record Mensaje(LocalDateTime fechaHora, String nombre, String mensaje) {
+	
+	@Override
+	public String toString() {
+		return "Mensaje [fechaHora=" + fechaHora + ", nombre=" + nombre + ", mensaje=" + mensaje + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(mensaje, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mensaje other = (Mensaje) obj;
+		return Objects.equals(mensaje, other.mensaje) && Objects.equals(nombre, other.nombre);
+	}
+	
+	public boolean esModeracion(){
+		return nombre == "Moderacion";
+	}
 	
 }
