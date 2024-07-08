04dda971ed5ac93ce3041172920eb298533b6bcf
dlozaco <dlozaco975@gmail.com>
2024-07-09 01:26:08 +0200

    Primer test hecho

--- Contador_Cagadas/src/test/cagadas/TestCagadas.java
+++ Contador_Cagadas/src/test/cagadas/TestCagadas.java
@@ -1,14 +1,28 @@
 package test.cagadas;
 
+import java.util.Map.Entry;
+
 import cagadas.Chat;
 import cagadas.FactoriaChat;
 
 public class TestCagadas {
 
 	public static void main(String[] args) {
-		String ruta = "data/Chat de WhatsApp con 💩";
+		String ruta = "inserta ruta";
 		Chat c = FactoriaChat.crearChat(ruta);
 		System.out.println(c);
+		testContarCacas(c);
 	}
-
+	
+	
+	private static void testContarCacas(Chat c) {
+		try {
+			System.out.println("\n---Test Contar Cacas---");
+			for(Entry<String, Long> par:c.contarCacas().entrySet()) {
+				System.out.println(par.getKey()+" --> "+par.getValue());
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
